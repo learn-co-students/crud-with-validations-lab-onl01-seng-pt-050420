d3ac509275fc6275dea6da778bcc7ac280622c2d
kanu-e <chukwuemekakanu@gmail.com>
2020-09-21 21:07:50 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,10 @@ class Song < ApplicationRecord
     validates :title, presence: true
     validates_presence_of :release_year, :if => :released?
     validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}, :if => :released?
-    validates :artist_name && :release_year && :title, uniqueness: true
+    validates :title, uniqueness: {
+        scope: [:release_year, :artist_name],
+        message: 'cannot be repeated by the same artist in the same year'
+      }
     validates :artist_name, presence: true
 
 end
