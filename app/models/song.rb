2ac2182a6a7938f19ff1aede98d254a470372e02
ryancdavey1 <ryancdavey1@gmail.com>
2020-09-21 21:37:39 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,22 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   validates :released, acceptance: { accept: [true, false ] }
-  # validate :invalid_release_year?
+  validates :artist_name, presence: true
 
-  # def invalid_release_year?
-  #   if @@clickbait.none? { |e| e.match title }
-  #     errors.add(:title, "post title must include clickbait")
-  #   end
-  # end
+  def released?
+    released
+  end
+
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
+  }
+  
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
+
 end
